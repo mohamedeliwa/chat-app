beb4da7c3e64eb3881a874bd5b757d7de5658d9d
mohamedeliwa <mohamed.av11@gmail.com>
2020-10-09 15:10:58 +0200

    adding title and buttons to home page

--- pages/index.js
+++ pages/index.js
@@ -9,22 +9,12 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className={styles.title}>Welcome to Chat App</h1>
 
       <main className={styles.main}>
-        <Button variant="primary">Join Rooms</Button>{" "}
-        <Button variant="primary">Create Room</Button>
+        <Button className={styles.roombtn} variant="primary">Join Rooms</Button>
+        <Button className={styles.roombtn} variant="primary">Create Room</Button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
