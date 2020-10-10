8661b6b2cf679d7f6f5f895d55c5d0d0273a5ba7
mohamedeliwa <mohamed.av11@gmail.com>
2020-10-10 12:56:36 +0200

    messages input added

---
+++ components/MsgBoard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MsgBoard: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Message Board</h1>
+    </div>
+  );
+};
+
+export default MsgBoard;
