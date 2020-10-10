8da5d5692d42f5f7024227cd479c6bdb2fd89477
mohamedeliwa <mohamed.av11@gmail.com>
2020-10-10 10:55:29 +0200

    responsive navbar created

---
+++ components/Navbar.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Link from "next/link";
+import { Nav } from "react-bootstrap";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import styles from "../styles/Navbar.module.scss";
+
+const Navbar = () => {
+  const [state, setState] = useState("open");
+
+  const handleState = (e) => {
+    e.preventDefault();
+    setState(state === "open" ? "collapse" : "open");
+  };
+
+  return (
+    <div
+      className={
+        state === "open"
+          ? styles.navbarContainer_opened
+          : styles.navbarContainer_collapsed
+      }
+    >
+      {state === "collapse" ? (
+        <GiHamburgerMenu className={styles.burgerbtn} onClick={handleState} />
+      ) : (
+        <AiOutlineClose className={styles.burgerbtn} onClick={handleState} />
+      )}
+      <Nav defaultActiveKey="/home" className={`flex-column ${styles.nav}`}>
+        <Link href="/">
+          <Nav.Link className={styles.navLink} href="/">
+            Lobby
+          </Nav.Link>
+        </Link>
+        <Link href="/">
+          <Nav.Link className={styles.navLink} href="/">
+            Games
+          </Nav.Link>
+        </Link>
+        <Link href="/">
+          <Nav.Link className={styles.navLink} href="/">
+            Friendship
+          </Nav.Link>
+        </Link>
+      </Nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ pages/rooms.tsx
@@ -0,0 +1,12 @@
+import  Navbar  from "../components/Navbar"
+
+const Rooms = () => {
+  return (
+    <div style={{position: 'relative'}}>
+      <h1> Hello Roooms</h1>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Rooms;
