90e2d2b920c19daeb48877add95bc947e8910e55
mohamedeliwa <mohamed.av11@gmail.com>
2020-10-20 11:19:44 +0200

    user can copy url of a private room

--- components/ShareRoom.tsx
+++ components/ShareRoom.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { Alert } from "react-bootstrap";
 import { MdShare } from "react-icons/md";
@@ -7,11 +8,25 @@ import { FiCopy } from "react-icons/fi";
 import styles from "../styles/ShareRoom.module.scss";
 
 const ShareRoom: React.FunctionComponent = () => {
+  const router = useRouter();
   const [state, setState] = useState<string>("collapse");
 
   const handleState = (e: React.MouseEvent) => {
     e.preventDefault();
     setState(state === "open" ? "collapse" : "open");
+  };
+
+  const copyLink = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const url = document.querySelector("#roomURL") as HTMLSpanElement;
+    const urlTextArea = document.createElement("textarea");
+    urlTextArea.value = url.innerText;
+    urlTextArea.style.position = "absolute";
+    urlTextArea.style.left = "-9999px";
+    document.body.appendChild(urlTextArea);
+    urlTextArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(urlTextArea);
   };
   return (
     <div
@@ -24,8 +39,8 @@ const ShareRoom: React.FunctionComponent = () => {
       ) : (
         <AiOutlineClose className={styles.sharebtn} onClick={handleState} />
       )}
-      <Alert className={styles.shareLink} variant="light">
-        http://localhost:3000/rooms?id=asshjsflhskjhfsjfnsjcnsjhfskjhfa
+      <Alert className={styles.shareLink} variant="light" onClick={copyLink}>
+        <span id="roomURL">{`http://localhost:3000${router.asPath}`}</span>
         <FiCopy className={styles.copyIcon} />
       </Alert>
     </div>
